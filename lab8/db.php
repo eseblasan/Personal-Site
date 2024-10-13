e84b8e503527e4b56d8976a73d73f9f6da9b990a
Illia_2034 <liadenko.ik@gmail.com>
2024-10-14 01:41:02 +0300

    33333333

--- lab8/db.php
+++ lab8/db.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $db = 'website_db';
 $user = 'root';
-$pass = 'your_password';
+$pass = 'your_password'; // Замість 'your_password' вкажіть свій пароль
 
 $conn = new mysqli($host, $user, $pass, $db);
 
